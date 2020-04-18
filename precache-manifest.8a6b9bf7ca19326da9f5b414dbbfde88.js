890703915ca59aab5eb4591a8a41e34beaae38fa
Artem <shtelzerartem@gmail.com>
2020-04-18 18:57:47 +0300

    Updates

---
+++ precache-manifest.8a6b9bf7ca19326da9f5b414dbbfde88.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82829a8eeea1991b1c551ce878241344",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb8eab9eee274e8b6215",
+    "url": "/static/css/main.297c3b88.chunk.css"
+  },
+  {
+    "revision": "680863abc4dbaeb078d5",
+    "url": "/static/js/2.0c184653.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c184653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8eab9eee274e8b6215",
+    "url": "/static/js/main.68a62c0c.chunk.js"
+  },
+  {
+    "revision": "6f5314cb3c6d2ef4a200",
+    "url": "/static/js/runtime-main.9d39c8ef.js"
+  },
+  {
+    "revision": "bcbec2432a39073ad238c6a05f676f8e",
+    "url": "/static/media/DrukWideCy-Bold-Desktop.bcbec243.otf"
+  },
+  {
+    "revision": "4a3627f6b440234bbb6543a660fc48cd",
+    "url": "/static/media/Epos-Normal-Trial.4a3627f6.otf"
+  },
+  {
+    "revision": "fef2b7ee1ccedb94731ac40c8aa43480",
+    "url": "/static/media/RF-Rostin-Regular.fef2b7ee.otf"
+  },
+  {
+    "revision": "be26b44a6e11e8bf9ec96c3a767dbf54",
+    "url": "/static/media/fire.be26b44a.svg"
+  },
+  {
+    "revision": "068c82701ca695611e4598b26fd0f4a1",
+    "url": "/static/media/logo.068c8270.svg"
+  }
+]);
